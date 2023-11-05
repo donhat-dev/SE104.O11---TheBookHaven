cf435ee3ff5af3d23b9e4dd9012fb0d9b3799b4d
donhat-dev <119946877+donhat-dev@users.noreply.github.com>
2023-11-06 00:30:29 +0700

    Create api_test.php

---
+++ api_test.php
@@ -0,0 +1,23 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'http://178.128.62.16:8069/api/v2/session',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => false,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'accept: application/json',
+    'Authorization: Basic YWRtaW46YWRtaW4='
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
